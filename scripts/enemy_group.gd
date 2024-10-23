63745fef447e8b8a755e1e61fbcce5a648561232
Waleed Lugod <lugodwaleed@gmail.com>
2024-10-24 02:29:49 +0800

    Allow different action types

--- scripts/enemy_group.gd
+++ scripts/enemy_group.gd
@@ -4,6 +4,7 @@ var enemies: Array = []
 var action_queue: Array  = []
 var is_battling: bool = false
 var index: int = 0
+var currentAction: String = ""
 
 signal next_player
 @onready var choice: VBoxContainer = $"../CanvasLayer/Choice"
@@ -14,8 +15,6 @@ func _ready() -> void:
 	for i in enemies.size():
 		enemies[i].position = Vector2(0, i*36)
 		
-	enemies[0].focus()
-	
 	show_choice()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,8 +31,9 @@ func _process(delta: float) -> void:
 				switch_focus(index, index-1)
 				
 		if Input.is_action_just_pressed("ui_accept"):
-			action_queue.push_back(index)
+			action_queue.push_back([currentAction, index])
 			emit_signal("next_player")
+			_reset_focus()
 			show_choice()
 		
 	if action_queue.size() == enemies.size() and not is_battling:
@@ -41,9 +41,17 @@ func _process(delta: float) -> void:
 		_action(action_queue)
 			
 func _action(stack):
-	for i in stack:
-		enemies[i].take_damage(1)
-		await get_tree().create_timer(1).timeout
+	for action in stack:
+		match action[0]:
+			"attack":
+				enemies[action[1]].take_damage(1)
+				await get_tree().create_timer(1).timeout
+			"defend": # defend logic
+				pass
+			"magic": # magic logic
+				enemies[action[1]].take_damage(3)
+				await get_tree().create_timer(1).timeout
+				pass
 	action_queue.clear()
 	is_battling = false
 	show_choice()
@@ -67,5 +75,15 @@ func _start_choosing():
 
 
 func _on_attack_pressed() -> void:
+	currentAction = "attack"
+	choice.hide()
+	_start_choosing()
+
+func _on_defend_pressed() -> void:
+	action_queue.push_back(["defend"])
+	emit_signal("next_player")
+
+func _on_magic_pressed() -> void:
+	currentAction = "magic"
 	choice.hide()
 	_start_choosing()
