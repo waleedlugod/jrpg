8e55eb630a0f493fff0c6c5132a20823efa53a56
Waleed Lugod <lugodwaleed@gmail.com>
2024-10-24 18:53:46 +0800

    refactor to to use game handler (battle_scene.gd)

--- scripts/battle_scene.gd
+++ scripts/battle_scene.gd
@@ -1,11 +1,104 @@
 extends Node2D
 
+@onready var action_choice: VBoxContainer = $"./CanvasLayer/Choice"
+@onready var enemies = $EnemyGroup.enemies
+@onready var players = $PlayerGroup.players
 
-# Called when the node enters the scene tree for the first time.
+
+# handles target focus when attacking
+var action_target: int = 0:
+	set(new_action_target):
+		enemies[action_target].unfocus()
+		enemies[new_action_target].focus()
+		action_target= new_action_target
+
+
+# handles player currently taking action
+var current_player: int = 0:
+	set(new_current_player):
+		players[current_player].unfocus()
+		players[new_current_player].focus()
+		current_player= new_current_player
+
+
+var action_queue: Array = []
+var chosen_action: String = ""
+var enemy_action_queue: Array = []
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+
+var is_battling: bool = false
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	show_choice()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	if not action_choice.visible:
+		if Input.is_action_just_pressed("ui_up"):
+			if action_target > 0:
+				action_target -= 1
+				
+		if Input.is_action_just_pressed("ui_down"):
+			if action_target < enemies.size() - 1:
+				action_target += 1
+				
+		if Input.is_action_just_pressed("ui_accept"):
+			action_queue.push_back({
+				chosen_action = chosen_action,
+				target = action_target
+			})
+			if current_player + 1 < players.size():
+				current_player += 1
+			clear_action_target_focus()
+			show_choice()
+
+	if action_queue.size() == players.size() and not is_battling:
+		is_battling = true
+		start_battle_sequence()
+
+
+func start_battle_sequence():
+	for action in action_queue:
+		print(action)
+		match action.chosen_action:
+			"attack": enemies[action.target].take_damage(1)
+			"defend": pass
+			"magic": pass
+	is_battling = false
+	current_player = 0
+	action_queue.clear()
+
+func generate_enemy_actions():
 	pass
+
+
+func show_choice():
+	action_choice.show()
+	action_choice.find_child("Attack").grab_focus()
+
+
+func clear_action_target_focus() -> void:
+	for enemy in enemies: enemy.unfocus()
+
+
+func start_choose_action_target():
+	action_choice.hide()
+	clear_action_target_focus()
+	action_target = 0
+
+
+func _on_attack_pressed() -> void:
+	start_choose_action_target()
+	chosen_action = "attack"
+
+
+func _on_defend_pressed() -> void:
+	action_queue.push_back({chosen_action = "defend"})
+	players[current_player].is_defending = true
+	if current_player + 1 < players.size(): current_player += 1
+
+
+func _on_magic_pressed() -> void:
+	chosen_action = "magic"
+	start_choose_action_target()
--- scripts/enemy_group.gd
+++ scripts/enemy_group.gd
@@ -1,92 +1,9 @@
 extends Node2D
 
 var enemies: Array = []
-var action_queue: Array  = []
-var is_battling: bool = false
-var index: int = 0
-var currentAction: String = ""
-var rng = RandomNumberGenerator.new()
-
-signal next_player
-signal reset_defend
-signal defend
-@onready var choice: VBoxContainer = $"../CanvasLayer/Choice"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	enemies = get_children()
 	for i in enemies.size():
 		enemies[i].position = Vector2(0, i*36)
-		
-	show_choice()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if not choice.visible:
-		if Input.is_action_just_pressed("ui_up"):
-			if index > 0:
-				index -= 1
-				switch_focus(index, index+1)
-				
-		if Input.is_action_just_pressed("ui_down"):
-			if index < enemies.size() - 1:
-				index += 1
-				switch_focus(index, index-1)
-				
-		if Input.is_action_just_pressed("ui_accept"):
-			action_queue.push_back([currentAction, index])
-			emit_signal("next_player")
-			_reset_focus()
-			show_choice()
-		
-	if action_queue.size() == enemies.size() and not is_battling:
-		is_battling = true
-		_action(action_queue)
-			
-func _action(stack):
-	for action in stack:
-		match action[0]:
-			"attack":
-				enemies[action[1]].take_damage(1)
-				await get_tree().create_timer(1).timeout
-			"magic": # magic logic
-				enemies[action[1]].take_damage(rng.randf_range(0, 5))
-				await get_tree().create_timer(1).timeout
-				pass
-	action_queue.clear()
-	emit_signal("reset_defend")
-	is_battling = false
-	show_choice()
-
-func switch_focus(x,y):
-	enemies[x].focus()
-	enemies[y].unfocus()
-	
-func show_choice():
-	choice.show()
-	choice.find_child("Attack").grab_focus()
-	
-func _reset_focus():
-	index = 0
-	for enemy in enemies:
-		enemy.unfocus()
-		
-func _start_choosing():
-	_reset_focus()
-	enemies[0].focus()
-
-
-func _on_attack_pressed() -> void:
-	currentAction = "attack"
-	choice.hide()
-	_start_choosing()
-
-func _on_defend_pressed() -> void:
-	action_queue.push_back(["defend"])
-	emit_signal("defend")
-	emit_signal("next_player")
-
-func _on_magic_pressed() -> void:
-	currentAction = "magic"
-	choice.hide()
-	_start_choosing()
